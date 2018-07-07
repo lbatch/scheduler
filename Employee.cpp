e9587b3c8bf8f00db0b1a87e2f3cbc296f379498
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-07-07 12:25:15 -0500

    Employee implementation

---
+++ Employee.cpp
@@ -0,0 +1,84 @@
+#include "Employee.h"
+
+int Employee::getId()
+{
+    return _id;
+}
+
+string Employee::getName()
+{
+    return _name;
+}
+
+int Employee::getHours()
+{
+    return _hoursAssigned;
+}
+
+bool Employee::checkAvailable(int slot)
+{
+    if(_availability[slot] == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Employee::checkAssigned(int slot)
+{
+    if(_assigned[slot] == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Employee::setId(int newId)
+{
+    _id = newId;
+}
+
+void Employee::setName(string newName)
+{
+    _name = newName;
+}
+
+void Employee::setMin(int min)
+{
+    _minHours = min;
+}
+
+void Employee::setMax(int max)
+{
+    _maxHours = max;
+}
+
+void Employee::addAvailability(int slot)
+{
+    _availability[slot] = 1;
+}
+
+void Employee::removeAvailability(int slot)
+{
+    _availability[slot] = 0;
+}
+
+void Employee::addAssigned(int slot)
+{
+    _assigned[slot] = 1;
+}
+
+void Employee::removeAssigned(int slot)
+{
+    _assigned[slot] = 0;
+}
+
+void Employee::displaySchedule()
+{
+    for(int i = 0; i < MAX_SLOTS; i++)
+    {
+        if(_assigned[i] == 1)
+            cout << i << ' ';
+    }
+    cout << endl;
+}
